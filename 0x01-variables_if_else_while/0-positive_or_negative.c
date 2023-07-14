051283992d3f1b6a02851dd6aaf8a3b1422ea962
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-14 21:31:38 +0100

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 		{
 			printf("%d is negative",n);
 		}
-		if (n=0)
+		else if (n==0)
 	        {
 	                printf("%d is zero",n);
                 }
-		if (n>0)
+		else if (n>0)
                 {
-                        printf("%d is positive",n);
+                        printf("%d is positive",i);
                 }
 		return (0);
 }
