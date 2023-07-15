69926aec6eae08e00b30a04e560ae24c50c0b01d
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-15 03:43:35 +0100

    7

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num[9]="123456789"
+	int i;
+	for (i = 0; i < 10; i++)
+	{	
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
