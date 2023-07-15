0fa40735f77c5aeff56e1003133d36d97202d476
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-07-15 02:38:41 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,6 +12,7 @@ int main(void)
 {
 	int n;
 	int x;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
@@ -27,6 +28,5 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
-	
 	return (0);
 }
