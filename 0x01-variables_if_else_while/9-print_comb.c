e96cb53ca364db810d8c1e983a228d35f256faa6
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-15 04:06:07 +0100

    6

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -20,27 +19,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-=======
-#include <stdio.h>
-
-/**
- * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
-}
->>>>>>> 75e4abc47b50b220356e4c32094d60c6e5d563f6
