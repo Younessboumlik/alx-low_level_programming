1567826c78a7b41b6ec3c108f8c882a0b5eb9d1f
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-07-18 21:54:09 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,27 @@
-#include "main.h"
+#include <stdio.h>
+
+void print_alphabet(void); // Function prototype
 
 /**
- * main - check the code
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
-	print_alphabet();
-	return (0);
+    print_alphabet();
+    return 0;
 }
 
-void print_alphabet()
+/**
+ * print_alphabet - Prints the English alphabet in lowercase
+ */
+void print_alphabet(void)
 {
-	int i ;
-	    
-	for(i = 97;i <= 122 ;i++)
-	{
-		putchar(i);
-	}			  
+    int i;
+
+    for (i = 97; i <= 122; i++)
+    {
+        putchar(i);
+    }
+}
