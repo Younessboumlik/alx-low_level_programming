1dca8fb9a849b4c838eeec9cd69980b6781354e9
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-07-18 22:00:23 +0100

    Update 1-alphabet.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-void print_alphabet(void) {
-    int i;
-
-    for (i = 97; i <= 122; i++) {
-        putchar(i);
-    }
-}
+void print_alphabet(void);
 
 /**
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
     print_alphabet();
     return 0;
+}
+
+/**
+ * print_alphabet - Prints the English alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+    int i;
+
+    for (i = 97; i <= 122; i++)
+    {
+        putchar(i);
+    }
 }
