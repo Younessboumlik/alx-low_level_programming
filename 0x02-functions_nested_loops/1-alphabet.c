0f3621d74ab890356dfbdf3375486f090ce06450
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-18 21:36:53 +0100

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
-/*
- *main-blablabla
- **
- *wooork
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 
-int main()
+void print_alphabet(void):
 {
 	int i ;
-	        
+	    
 	for(i = 97;i <= 122 ;i++)
 	{
 		putchar(i);
-	}
+	}			  
+}
+
+int main(void)
+{
+	    print_alphabet();
+	        return (0);
 }
