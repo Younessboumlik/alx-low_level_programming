4b942003f309f311bc03ed3fd14c0801687e8a08
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-07-18 21:43:36 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,6 +6,13 @@
  * Return: Always 0.
  */
 
+
+int main(void)
+{
+	    print_alphabet();
+	        return (0);
+}
+
 void print_alphabet()
 {
 	int i ;
@@ -14,10 +21,4 @@ void print_alphabet()
 	{
 		putchar(i);
 	}			  
-}
-
-int main(void)
-{
-	    print_alphabet();
-	        return (0);
 }
