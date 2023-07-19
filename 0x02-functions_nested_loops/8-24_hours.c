0818523b7bbbf87a6e72a1c4b10a818e5735d94c
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-07-19 03:44:36 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,9 +2,9 @@
 
 
 /**
- * 	 * jack_bauer - prints every minute of the day of Jack Bauer
- * 	 	 * starting from 00:00 to 23:59
- */
+* 	 * jack_bauer - prints every minute of the day of Jack Bauer
+* 	 	 * starting from 00:00 to 23:59
+*/
 void jack_bauer(void)
 {
 	int i, j;
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	i = 0;
 
 	while (i < 24)
-				
+
 	{
 		j = 0;
 		while (j < 60)
