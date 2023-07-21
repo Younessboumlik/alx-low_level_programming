5a261bcf9140bc173823659d17622a922013acc5
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-21 03:03:01 +0100

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9.
+ *
+ * Return: 0123456789
+ */
+
+void print_numbers(void)
+{
+	for(i=0;i<10;i++)
+	{
+		_putchar(i);
+	}
+	_putchar("/n")
+}
