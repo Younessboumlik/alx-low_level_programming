6107a80b5cda4a6f4978d2f7916b572956da3bbb
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-21 02:33:09 +0100

    2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_isdigit - if c is digit [0,9]
+ *
+ * @c: the digit
+ * Return: 1 ou 0
+ */
+
+int int_isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
