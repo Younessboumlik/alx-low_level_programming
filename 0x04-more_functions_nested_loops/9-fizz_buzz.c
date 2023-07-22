346f67a10aec646c777bf6664b7bbf8c27016f98
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-07-22 03:00:39 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,23 +19,23 @@ int main(void)
 		
 		if ((i % 3 == 0) && (i % 5 == 0))
         	{	
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
         	{
-            		printf("Fizz");
+            		printf("Fizz ");
         	}
         	else if (i % 5 == 0)
         	{
             		printf("Buzz");
+			if (i != 100)
+			{
+            			printf(" ");
+        		}
         	}
         	else
         	{
-            		printf("%d", i);
-        	}
-        	if (i != 100)
-		{
-            		printf(" ");
+            		printf("%d ", i);
         	}
 	}
 	return (0);
