a115d4ff29af96e0d98190fe97474701a24b179c
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-25 03:11:19 +0100

    2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** 
+ * _puts - print with a new line.
+ *
+ * @str: the string that we should print.
+ * Return: The string with new line.
+ */
+
+void _puts(char *str)
+{
+	_putchar(str);
+	_putchar("\n")
+}
