117f2b1d7d84c46f5bed6a29e74d2379ef7c55f6
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-26 03:36:14 +0100

    4

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,14 +18,14 @@ void puts_half(char *str)
 	}
 	if (n % 2 == 0)
 	{
-		for(i = n / 2; i < n; i++)
+		for (i = n / 2; i < n; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = ( (n - 1) / 2) + 1; i < n; i++)
+		for (i = ((n - 1) / 2) + 1; i < n; i++)
 		{
 			_putchar(str[i]);
 		}
