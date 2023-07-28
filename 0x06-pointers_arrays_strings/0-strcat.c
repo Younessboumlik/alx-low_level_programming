5a0977ecbb11b795b3829a5a41e3923ca4768a76
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-28 01:37:30 +0100

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,30 @@
 #include "main.h"
 
 /**
- * *_strcat - 
+ * _strcat - string after a string.
+ *
+ * @dest: string.
+ * @src: string.
+ * Return: string with the two strings.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n = 0,m = 0;
+	char *res;
+
+	while (*src != "\0")
+	{
+		*res=*src;
+		res=res+1;
+		src=src+1;
+	}
+	while (*dest != "\0")
+	{
+		*src=*src+*des;
+		src=src+1;
+		dest=dest+1;
+	}
+	return (src);
+	
+}
