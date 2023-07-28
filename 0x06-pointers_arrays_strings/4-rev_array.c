3928d9d062ce646593a23f4191e9f5cf9f6d8502
Younessboumlik <younessboumlik1949@gmail.com>
2023-07-28 03:58:41 +0100

    f

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* reverse_array - reverse an array
+ *
+ * @a: the array of integers.
+ * @n: the number of the elements in the array.
+ * Return: the array but reversed.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *b;
+
+	for ((i = n - 1) && (j = 0); (i >= 0) && (j <= n-1) ; (i--) && (j++))
+	{
+		*(b + j)=(a + i);
+	}
+	return (b);
+}
