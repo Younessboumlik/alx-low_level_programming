5a7dc7363ce084994825a4083a8af056557de60e
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-07-28 01:53:08 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,17 +12,16 @@ char *_strcat(char *dest, char *src)
 {
 	int n = 0,m =0;
 
+	while (*dest != '\0')
+	{
+		dest=dest+1;
+	}
+	
 	while (*src != '\0')
 	{
-		while (*dest != '\0')
-		{
-			dest=dest+1;
-		}
-	}
-
-	*dest=*src;
-	src=src+1;
-	dest=dest+1;
+		*dest=*src;
+		src=src+1;
+		dest=dest+1;
 	}
 
 	return (dest);
