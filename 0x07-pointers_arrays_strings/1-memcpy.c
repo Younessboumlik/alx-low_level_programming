8052f309db4524873d4521ba7c6f2e3a1d8c63e2
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-01 01:47:35 +0100

    2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copy memory area.
+ *
+ * @dest: the destination.
+ * @src: the source.
+ * @n: the number of bites.
+ */
+
+*_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = n; i > 0; i--)
+	{
+		dest[n-i] = source[n-i];
+	}
+	return (dest);
+}
