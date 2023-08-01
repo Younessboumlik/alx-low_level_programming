8fa832f232bd6cc8226064894190db3afd274ba1
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-01 03:42:17 +0100

    3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,15 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == c)
-		{
-			return (&(s[i]));
-			break;
-		}
-		i++;
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
-	return (NULL);
+	if (c == '\0')
+		return (s);
+
+				return (NULL);
 }
