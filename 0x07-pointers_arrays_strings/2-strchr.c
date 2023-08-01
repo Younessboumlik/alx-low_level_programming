a5d05e82ee5515100aa1ac52619d229c5e4b2018
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-01 02:19:18 +0100

    3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strchr - Returns a pointer to the first occurrence of the character.
+ *
+ * @s: the source.
+ * @c: the character.
+ * Return: the pointer in c.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *res;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			res = &s;
+			break;
+		}
+		i++;
+	return (res);
+	}
+}
