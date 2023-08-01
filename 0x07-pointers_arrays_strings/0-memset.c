40f5ae8be2f5cab4a4ba80dc30e72fc49dc8ba1a
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-01 01:37:58 +0100

    2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * *_memset - fills first n bytes.
+ * * _memset - fills first n bytes.
  *
  * @s: the pointer of a char.
  * @b: the char that we will remplace in s with n bites.
  * @n: number of bits.
+ * Return: the new character.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i=n;i>0;i--)
+	for (i = n; i > 0; i--)
 	{
-		s[n-i]=b;
+		s[n-i] = b;
 	}
 	return (s);
 }
