1cbd5543d2067941344b77135e10a8d2b61e2dd5
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-01 22:42:41 +0100

    4

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - point a pointer to a char.
+ *
+ * @s: the pointer that we should point.
+ * @to: the char that should be pointed.
+ * Return: nothing, just point *s at to.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s=to;
+}
