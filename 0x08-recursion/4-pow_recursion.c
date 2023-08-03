cb45389e4ab09bcad4618f34046941a7b1725b26
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-03 03:54:15 +0100

    2

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - x power y.
+ *
+ * @x: a int
+ * @y: a int
+ * Return: x**y.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x *  _pow_recursion(x,y - 1))
+	}
+}
