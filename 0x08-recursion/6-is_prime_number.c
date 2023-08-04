1a7f2ae3356a14ece021df88cabcc76ca4f84f84
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-04 03:53:45 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,12 +2,13 @@
 #include "main.h"
 
 /**
- * check - checks to see if number is prime
- * @a:int
- * @b:int
- * Return:int
+ * verefication - checks to see if number is prime.
+ *
+ * @a:int.
+ * @b:int.
+ * Return:int.
  */
-int check(int a, int b)
+int verefication(int a, int b)
 {
 	if (b < 2 || b % a == 0)
 		return (0);
@@ -18,13 +19,14 @@ int check(int a, int b)
 }
 
 /**
- * is_prime_number - states if number is prime
- * @n:int
- * Return:int
+ * is_prime_number - states if number is prime.
+ *
+ * @n:int.
+ * Return:int.
  */
 int is_prime_number(int n)
 {
 	if (n == 2)
 		return (1);
-	return (check(2, n));
+	return (verefication(2, n));
 }
