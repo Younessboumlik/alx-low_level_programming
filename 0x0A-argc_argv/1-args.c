9790ef4fa9eb2a597fc9d80b4b54f80907f3ca4b
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-04 23:40:43 +0100

    3

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - the number of arguments.
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n",argc);
+	return (0);
+}
