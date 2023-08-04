55b36d365555ba64286a3f16deb7c98b95d150ca
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-04 17:29:12 +0100

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - the name of the program.
+ *
+ * @argc: the number of arguments.
+ * @argv: the value of arguments.
+ */
+
+int main(int argc(), char **argv())
+{
+	printf("%s\n",)
+}
