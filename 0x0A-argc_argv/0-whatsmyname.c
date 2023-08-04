3e1e7c9621cdb26fd59f5f46cc30129564f91f21
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-04 22:54:39 +0100

    3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,7 +8,7 @@
  * @argv: the value of arguments.
  */
 
-int main(int argc(), char **argv())
+int main(int argc, char **argv)
 {
 	printf("%s\n",argv[0]);
 	return (0);
