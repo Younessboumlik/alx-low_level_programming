6626bf927950983eec3f21c92fc89f91216e1db4
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-05 01:20:09 +0100

    4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the mult of two int.
+ *
+ * @argc: the count of args.
+ * @argv: the values of args.
+ * Return: the mult of 2 int.
+ */
+
+int main(int argc, char **argv)
+{
+	int x,y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x=atoi(argv[1]);
+	y=atoi(argv[2]);
+	printf("%d\n", x * y);
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -7,5 +7,6 @@ int main(int argc, char **argv);
 int main(int argc, char **argv);
 int main(int argc, char **argv);
 int main(int argc, char **argv);
+int main(int argc, char **argv);
 
 #endif
