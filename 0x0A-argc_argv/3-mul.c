3add2194c13a2390803cb2e83083e2eb16eecada
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-05 01:23:56 +0100

    4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +12,7 @@
 
 int main(int argc, char **argv)
 {
-	int x,y;
+	int x, y;
 
 	if (argc < 3)
 	{
@@ -21,6 +22,7 @@ int main(int argc, char **argv)
 
 	x=atoi(argv[1]);
 	y=atoi(argv[2]);
+
 	printf("%d\n", x * y);
 
 	return (0);
