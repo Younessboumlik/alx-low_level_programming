cfc9826942761446377a8ded7f09aedd417d8570
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-07 17:49:54 +0100

    3

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *create_array - create an array of char.
+ *
+ * @size: the size of the array.
+ * @c: the char that we will put in the array.
+ * Return: The pointer, or NULL.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *table;
+	table = malloc(sizeof(char) * (size));
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i=0; i=size-1; i++)
+		{
+			table[i] = c[i];
+		}
+	}
+	return table;
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+readme file, in this projects I will practice the malloc and free functions.
