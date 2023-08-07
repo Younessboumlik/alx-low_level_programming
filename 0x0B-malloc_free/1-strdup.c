899d7cf900e9860ec65191ef732359b727b69cc6
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-07 18:45:37 +0100

    edit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strdup - copy an integer using malloc function.
+ *
+ * @str: the string giving bu the user.
+ * Return: a pointer in the copy of the string.
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	int n = 0,i;
+
+	while(*str)
+	{
+		n = n + 1;
+		str = str + 1;
+	}
+	new_str = malloc(sizeof(char)*n)
+	if ((str == NULL) || (new_str == NULL))
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			new_str[i] = str[i];
+		}
+	}
+	return (new_str);
+}
