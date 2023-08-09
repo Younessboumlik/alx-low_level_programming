f9039c0ac6c310a1875f10d5998347f85ea03918
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-09 17:43:39 +0100

    task 100:

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * argstostrm - concatenates the arguments/
+ * argstostr - concatenates the arguments/
  *
  * @ac: an integer.
  * @av: a pointer that point in a pointer that point in a char.
@@ -10,14 +10,14 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, total_lenght = 0, current_position = 0;
+	int i, total_length = 0, current_position = 0;
 	char *result;
 
 	if ((ac == 0) || (av ==NULL))
 	{
 		return (NULL);
 	}
-	char *result = (char *)malloc(total_length * sizeof(char));
+	*result = (char *)malloc(total_length * sizeof(char));
 
 	if (result == NULL) 
 	{
@@ -26,7 +26,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++) 
 	{
 		strcpy(result + current_position, av[i]);
-		current_position += strlen(av[i]);
+		current_position = strlen(av[i]) + current_position;
 		result[current_position++] = '\n';
 	}
 	return (result);
