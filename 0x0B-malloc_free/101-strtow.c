37c2182cbc1a19f283692d9faba047d5d3a3af66
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-09 20:50:59 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,56 +8,49 @@
 
 char **strtow(char *str)
 {
-	int n = 0, i = 0, j = 0, k = 0, lengh = 0;
-	char **result;
+	char **words;
+	int i, j, k, len, count;
+	int word_start, word_end;
 
-	if ((str == NULL) || (strcmp(str, "") == 0))
-	{
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
-	}
 
-	while(str[i] != '\0')
-	{
-		if ((str[i] != ' ') && ((i == 0) || (str[i - 1] == ' ')))
-		{
-			n++;
-		}
-		i++;
-	}
+	/* Count the number of words in the string */
+	for (i = 0, count = 0; str[i]; i++)
+		if ((str[i] != ' ' && str[i + 1] == ' ') ||
+			(str[i] != ' ' && str[i + 1] == '\0'))
+			count++;
 
-	result = (char **)malloc(sizeof(char *) * (n + 1));
-	if (result == NULL)
-	{
+	/* Allocate memory for the array of words */
+	words = malloc(sizeof(char *) * (count + 1));
+	if (words == NULL)
 		return (NULL);
-	}
 
-	i = 0;
-	while(str[i] != '\0')
+	/* Split the string into words */
+	for (i = 0, j = 0; i < count; i++)
 	{
-		if ((str[i] != ' ') && ((i == 0) || (str[i - 1] == ' ')))
-		{
-			k = i;
-			while ((str[i] != ' ') && (str[i] != '\0'))
-			{
-				i++;
-			}
-			lengh = i - k;
-			result[j] = malloc(sizeof(char) * (lengh + 1));
-			if (result[j] == NULL)
-			{
-				return (NULL);
-			}
-			strncpy(result[j], str + k, lengh);
-			result[j][lengh] = '\0';
+		/* Find the start and end indices of the next word */
+		while (str[j] == ' ')
 			j++;
- 		}
- 		else
- 		{
- 			i++;
- 		}
+		word_start = j;
+		while (str[j] != ' ' && str[j] != '\0')
+			j++;
+		word_end = j;
+
+		/* Calculate the length of the word and allocate memory for it */
+		len = word_end - word_start;
+		words[i] = malloc(sizeof(char) * (len + 1));
+		if (words[i] == NULL)
+			return (NULL);
+
+		/* Copy the word into the array of words */
+		for (k = 0; k < len; k++)
+			words[i][k] = str[word_start + k];
+		words[i][len] = '\0';
 	}
 
-	result[n] = NULL;
+	/* Add the NULL terminator to the array of words */
+	words[count] = NULL;
 
-	return (result);
+	return (words);
 }
