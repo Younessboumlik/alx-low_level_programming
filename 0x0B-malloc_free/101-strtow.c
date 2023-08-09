234de530187eecf68069a7708d30d3956920c3c7
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-09 20:43:53 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,39 +1,39 @@
-#include "main.h"
-
-/**
- * strtow - transoform a string to a table of chars.
- *
- * *str: the string.
- * Return: an array of strings.
- */
-
 char **strtow(char *str)
 {
-	int n = 1, i = 0, j = 0, k = 0, lengh = 0;
+	int n = 0, i = 0, j = 0, k = 0, lengh = 0;
 	char **result;
 
 	if ((str == NULL) || (strcmp(str, "") == 0))
 	{
 		return (NULL);
 	}
+
+	// Count the number of words
 	while(str[i] != '\0')
 	{
-		if (str[i] == ' ')
+		if ((str[i] != ' ') && ((i == 0) || (str[i - 1] == ' ')))
 		{
-			n = n + 1;
+			n++;
 		}
-		i = i + 1;
+		i++;
 	}
-	result = (char **) (malloc(sizeof(char *) * (n + 1)));
+
+	result = (char **)malloc(sizeof(char *) * (n + 1));
 	if (result == NULL)
 	{
 		return (NULL);
 	}
+
 	i = 0;
 	while(str[i] != '\0')
 	{
-		if(str[i] == ' ')
+		if ((str[i] != ' ') && ((i == 0) || (str[i - 1] == ' ')))
 		{
+			k = i;
+			while ((str[i] != ' ') && (str[i] != '\0'))
+			{
+				i++;
+			}
 			lengh = i - k;
 			result[j] = malloc(sizeof(char) * (lengh + 1));
 			if (result[j] == NULL)
@@ -42,19 +42,14 @@ char **strtow(char *str)
 			}
 			strncpy(result[j], str + k, lengh);
 			result[j][lengh] = '\0';
-			j = j + 1;
-			k = i + 1;
+			j++;
  		}
-		i = i + 1;
+ 		else
+ 		{
+ 			i++;
+ 		}
 	}
-	lengh = i - k;
-	result[j] = (char *)malloc(sizeof(char) * (lengh + 1));
-	if (result[j] == NULL)
-	{
-		return (NULL);
-	}
-	strncpy(result[j], str + k, lengh);
-	result[j][lengh] = '\0';
+
 	result[n] = NULL;
 
 	return (result);
