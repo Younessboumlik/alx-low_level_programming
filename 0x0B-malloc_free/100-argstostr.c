05f7ced0965e1673adfb26f00fa1f089ca5bc176
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-09 17:49:06 +0100

    task 100:

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,11 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * argstostr - concatenates the arguments/
+ * argstostr - concatenates the arguments.
  *
- * @ac: an integer.
- * @av: a pointer that point in a pointer that point in a char.
- * Return: the two arguments concatenainting.
+ * @ac: number of arguments.
+ * @av: array of argument strings.
+ * Return: the concatenated string or NULL if an error occurs.
  */
 
 char *argstostr(int ac, char **av)
@@ -13,20 +16,25 @@ char *argstostr(int ac, char **av)
 	int i, total_length = 0, current_position = 0;
 	char *result;
 
-	if ((ac == 0) || (av ==NULL))
+	if ((ac == 0) || (av == NULL))
 	{
 		return (NULL);
 	}
-	*result = (char *)malloc(total_length * sizeof(char));
+	for (i = 0; i < ac; i++)
+	{
+		total_length += strlen(av[i]) + 1;
+	}
 
-	if (result == NULL) 
+	result = (char *)malloc(total_length * sizeof(char));
+
+	if (result == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < ac; i++) 
+	for (i = 0; i < ac; i++)
 	{
 		strcpy(result + current_position, av[i]);
-		current_position = strlen(av[i]) + current_position;
+		current_position += strlen(av[i]);
 		result[current_position++] = '\n';
 	}
 	return (result);
