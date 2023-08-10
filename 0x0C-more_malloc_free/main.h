93eae74d86db9ba5b6adb8d968880b99003cacaa
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-08-10 14:03:52 +0100

    Mora malloc and calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 #include <stdio.h>
-#include <string.h>
-
+#include <stdlib.h>
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
-har *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif
+
+#endif/*MAIN_H*/
