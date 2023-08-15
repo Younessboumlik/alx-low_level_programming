55b04fc31b3724efc7a652cb6b0d4494d051ad94
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-15 03:23:39 +0100

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,9 +3,4 @@
 
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
-
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif
