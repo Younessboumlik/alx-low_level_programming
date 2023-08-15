9d131a46e96c6ea99e27207d6ee31cad8258e410
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-15 03:15:05 +0100

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - printf the name of file compiled.
+ *
+ * @void: just nothing.
+ * Return: print the name of file.
+ */
 
-void main(void)
+int main(void)
 {
 	printf("%s\n",__FILE__);
+	return (0);
 }
