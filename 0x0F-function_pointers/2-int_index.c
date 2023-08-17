ae9ff7596e7d80dc00c129a33adb46eb4a1247db
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-17 03:33:02 +0100

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,20 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		return (-1);
-	}
-	if (array && cmp)
-	{
-		for (; i < size; i++)
+		if (array != NULL && cmp != NULL)
 		{
-			if (cmp(array[i]) != 0)
+			for (; i < size; i++)
 			{
-				return (i);
+				if (cmp(*(array + i)) != 0)
+					return (i);
 			}
-			
 		}
-		return (-1);
 	}
+	return (-1);
 }
