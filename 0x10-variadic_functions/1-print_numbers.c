e6bfebf660dac985f2e46e5d5d8e98a4e8ad47dd
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-18 03:33:40 +0100

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,23 +17,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(ptr, n);
 	if (separator == NULL)
-	{
 		separator = "";
-	}
 
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ptr, char*);
 		if (str == NULL)
-		{
 			str = "(nil)";
-		}
 		printf("%s", str);
 		if (i < n - 1)
-		{
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
-	va_end(ptr);
+	va_end(ap);
 }
