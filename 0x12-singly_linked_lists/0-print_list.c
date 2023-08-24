d66e49c6eeb359ee9bbe5c9fe603b2a9a7755693
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-24 18:11:48 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,29 @@
-#include "main.h"
+#include "lists.h"
 
 /**
+ * print_list - print elements of the list.
  *
+ * @h: a list_t variable.
+ * Return: the numbre of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int j = 0;
+
+	if ((h -> str) == NULL)
+	{
+		printf("[0] (nil)\n");
+		j = j + 1;
+		h = h -> next;
+	}
+	while (h -> next != NULL)
+	{
+		j = j + 1;
+		printf("[%d] %s\n", h -> len, h -> str);
+		h = h -> next;
+	}
+	j = j + 1;
+	printf("[%d] %s\n", h -> len, h -> str);
+	return (j);
+}
