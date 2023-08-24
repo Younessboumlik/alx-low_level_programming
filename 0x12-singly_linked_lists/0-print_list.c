67e10f49c13e43178e4b566bb4bae87052fb4aee
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-24 18:44:07 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,17 +11,20 @@ size_t print_list(const list_t *h)
 {
 	size_t j = 0;
 
-	if ((h->str) == NULL)
-	{
-		printf("[0] (nil)\n");
-		j = j + 1;
-		h = h->next;
-	}
 	while (h->next != NULL)
 	{
-		j = j + 1;
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if ((h->str) == NULL)
+		{
+			printf("[0] (nil)\n");
+			j = j + 1;
+			h = h->next;
+		}
+		else
+		{
+			j = j + 1;
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
 	}
 	j = j + 1;
 	printf("[%d] %s\n", h->len, h->str);
