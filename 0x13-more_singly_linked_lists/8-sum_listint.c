b4ac9bb584fc02692ffb42638a08ddd7779383fc
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-28 21:48:45 +0100

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint: the sum of all the data.
+ *
+ * @head: the head if the list.
+ * Return: the sum of data.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum = sum + (head->n);
+		head = head->next;
+	}
+	return (sum);
+}
