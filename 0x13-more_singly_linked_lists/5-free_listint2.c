af4e979bb8549346b67a645c762c70b9ab8718d3
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-28 20:49:43 +0100

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
 	if (head == NULL)
 	{
 		return;
@@ -17,7 +18,7 @@ void free_listint2(listint_t **head)
 	while ((*head) != NULL)
 	{
 		temp = (*head)->next;
-		free (*head);
+		free(*head);
 		(*head) = temp;
 	}
 }
