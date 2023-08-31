bfc7622e2afd18ddb1b189bdc5b916e5b01f31f6
Younessboumlik <younessboumlik1949@gmail.com>
2023-08-31 21:37:14 +0100

    task 4

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - flip to get from one number to another.
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of bits you would need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int differnce, mask = 1;
+	unsigned int j = 0, i = 0;
+
+	differnce = n ^ m;
+	for (; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (result == (differnce & mask))
+			j++;
+		mask <<= 1;
+	}
+
+	return (j);
+}
