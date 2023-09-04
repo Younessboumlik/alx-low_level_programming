683fdddd6cf842beceb8dee6228eef640ccc7ebe
Younessboumlik <younessboumlik1949@gmail.com>
2023-09-05 00:39:11 +0100

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,20 +11,22 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int i = 0, o, w;
-	char *ch = text_content;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	while(*ch)
+	while (*text_content)
 	{
 		i = i + 1;
-		ch = ch + 1;
+		text_content = text_content + 1;
 	}
+	text_content = text_content - i;
 	o = open(filename, O_WRONLY | O_APPEND);
+	if (o == -1)
+		return (-1);
 	w = write(o, text_content, i);
-	if (o == -1 || w == -1)
+	if (w == -1)
 		return (-1);
 	close(o);
 	return (1);
