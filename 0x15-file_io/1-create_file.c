2fffa083441041f276563254a0473728032d9cb8
Younessboumlik <younessboumlik1949@gmail.com>
2023-09-04 22:46:37 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	write = write(open, text_content, i);
 	if (write == -1)
 		return (-1);
-	close = close(opened);
+	close = close(open);
 	if (close == -1)
 		return (-1);
 	return (1);
