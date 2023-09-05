cbce94ab325d4e65e9620742f3ef21f784595fa4
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-09-05 03:32:52 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void close_file(int fd, char *file);
+char *read_file(char *file);
+void close_file(int fd);
 int main(int argc, char *argv[]);
 #endif
