6d080cb3880a6a2e016f78e13f0caf66de6ff171
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-11-04 03:17:11 +0100

    Add files via upload

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stddef.h>
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *p;
+	p = malloc(sizeof(hash_table_t));
+	if (p == NULL){
+		return NULL;
+	}
+	p->size = size;
+	p->array = malloc(size*sizeof(hash_node_t *));
+	
+	return (p);
+}
