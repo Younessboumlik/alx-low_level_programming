a0fb74fdd1646bece628edc98bc5ba7088f6156d
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-11-04 03:26:18 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
-* hash_table_create - create hash table.
-**
-* Return: the new hash table.
-* size: the size of the new hash table.
-**/
+ * hash_table_create - create hash table.
+ **
+ * Return: the new hash table.
+ * size: the size of the new hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *p;
